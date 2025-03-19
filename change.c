b6b716abfe4580e0b52f13b717ad27ddc61b1de4
IshakMoore <imoore28@brophybroncos.org>
2025-03-19 21:06:39 +0000

    this sure is a change

--- change.c
+++ change.c
@@ -2,9 +2,39 @@
 
 int main(void){
     //Work here
-    printf("hello");
+    double myChange, totalCoins;
+    double quarters = 0;
+    double dimes = 0;
+    double nickels = 0;
+    double pennies = 0;
+    printf("how many cents do you got bucko (answer in whole numbers) ");
+    scanf("%lf", &myChange);
 
+while (myChange >= 25) { 
+    myChange = myChange - 25; 
+    quarters++; 
+}
+printf("Quarters given: %lf\n", quarters);
 
+while (myChange >= 10) { 
+    myChange = myChange - 10; 
+    dimes++; 
+}
+printf("Dimes given: %lf\n", dimes);
 
+while (myChange >= 5) { 
+    myChange = myChange - 5; 
+    nickels++; 
+}
+printf("Nickels given: %lf\n", nickels);
+
+while (myChange >= 1) { 
+    myChange = myChange - 1; 
+    pennies++; 
+}
+printf("Pennies given: %lf\n", pennies);
+
+totalCoins = quarters += dimes += nickels += pennies;
+printf("Total coins given: %lf\n", totalCoins);
     return 0;
 }
